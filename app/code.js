394fd534e178a383d4977615d563ed9d5016e78c
Constantin Bogdan-Teodor <bogdan.constantin0110@gmail.com>
2023-06-09 16:40:26 +0300

    New commands
    Now you can add a floating number and you can delete the entire "process" with C(Clean) command on the calculator side.

--- app/code.js
+++ app/code.js
@@ -1,3 +1,30 @@
 let screen_text = document.querySelector("#screen_text");
+const buttons = document.querySelectorAll("button");
 
-let button_1 = document.querySelector("#button_1");
+let a = 0;
+let b = 0;
+let dot = '.'
+
+buttons.forEach(b => b.onclick = e => 
+    {
+        if(e.target.value <= '9' && e.target.value >= '0')
+            {
+
+                screen_text.innerHTML += e.target.value;
+                a = parseFloat(screen_text.innerHTML);
+            }
+        
+        else if(screen_text.innerHTML.trim() !== '' && e.target.value === '.' &&  !screen_text.innerHTML.includes(dot)) {
+            screen_text.innerHTML += e.target.value;
+        }
+        else if(e.target.value === 'C') {
+            screen_text.innerHTML = '';
+            a = 0;
+        }
+        else if(e.target.value === '+') {
+            screen_text.innerHTML = '';
+        }
+            console.log(a);
+            console.log(typeof a);
+        
+    });
