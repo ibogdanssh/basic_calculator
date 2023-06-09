3b0741e1db88828b7f62c0ba3f3593bc34ccb1ef
Constantin Bogdan-Teodor <bogdan.constantin0110@gmail.com>
2023-06-09 17:22:36 +0300

    add  new operations
    you can calculate just 2 numbers, in the future will be implemented a string of calculations ex(5+3*3/5)

--- app/code.js
+++ app/code.js
@@ -2,7 +2,10 @@ let screen_text = document.querySelector("#screen_text");
 const buttons = document.querySelectorAll("button");
 
 let a = 0;
+let temp = 0;
 let dot = '.'
+let result;
+let operator;
 
 buttons.forEach(b => b.onclick = e => 
     {
@@ -20,8 +23,28 @@ buttons.forEach(b => b.onclick = e =>
             screen_text.innerHTML = '';
             a = 0;
         }
-        else if(e.target.value === '+') {
+        else if(e.target.value === '+' || e.target.value === 'x' || e.target.value === '/' || e.target.value === '-') {
+            operator = e.target.value;
             screen_text.innerHTML = '';
+            temp = a;
+        }
+        else if(e.target.value === '=' && temp != 0) {
+            switch(operator) {
+                case '+':
+                    temp += a;
+                    break;
+                case '-':
+                    temp -= a;
+                    break;
+                case 'x':
+                    temp *= a;
+                    break;
+                case '/':
+                    temp /= a;
+                    break;
+
+            }
+            screen_text.innerHTML = temp.toFixed(5);
         }
             console.log(a);
             console.log(typeof a);
